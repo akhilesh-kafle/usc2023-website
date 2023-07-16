f98d200a00c7910f07c9fcef41b424da8802b6c8
akhilesh-kafle <akhileshkafle@gmail.com>
2023-07-16 19:38:14 +0545

    responsive-navbar

--- js/script.js
+++ js/script.js
@@ -22,3 +22,23 @@ window.addEventListener("click",function(){
     this.document.querySelector(".preloader").style.display = "none";
 })
 
+
+
+window.addEventListener("scroll",function(){
+    if (window.scrollY>30){
+        document.querySelector(".navbar").classList.add("sticky");
+    }
+    else{
+        document.querySelector(".navbar").classList.remove("sticky");
+    }
+})
+
+const togglebtn = document.querySelector(".toggle-btn");
+const togglebtnic = document.querySelector(".togle-btn i")
+
+togglebtn.addEventListener("click", function(){
+    document.querySelector(".dropdown").classList.toggle("click");
+})
+
+
+
