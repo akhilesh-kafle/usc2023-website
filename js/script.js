b3b8adfe996dd002effc200e9ec08c25a3a45dbb
akhilesh-kafle <akhileshkafle@gmail.com>
2023-08-15 01:58:32 +0545

    edit

--- js/script.js
+++ js/script.js
@@ -42,3 +42,68 @@ togglebtn.addEventListener("click", function(){
 
 
 
+//popup image
+const popupButton = document.getElementById("popupButton");
+  const imageContainer = document.getElementById("imageContainer");
+  
+  popupButton.addEventListener("click", () => {
+    imageContainer.style.display = "flex";
+});
+  
+  imageContainer.addEventListener("click", () => {
+    imageContainer.style.display = "none";
+});
+
+//facebook api
+
+// window.fbAsyncInit = function() {
+//     FB.init({
+//       appId: 'YOUR_APP_ID',
+//       autoLogAppEvents: true,
+//       xfbml: true,
+//       version: 'v12.0'
+// });
+// };
+  
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+
+//scroll animation
+
+const hidden = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+hidden.forEach((element)=>{
+    observer.observe(element);
+});
+
+// Scroll Progress Bar
+window.onscroll = function() { scrollProgress() };
+
+function scrollProgress() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+    if (scrolled > 80) {
+        document.getElementById("goUp").style.display = "block";
+    } else {
+        document.getElementById("goUp").style.display = "none";
+    }
+}
