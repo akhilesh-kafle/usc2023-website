6add30556049625dfef453482c997ca69fa8de38
akhilesh-kafle <akhileshkafle@gmail.com>
2023-08-17 20:26:36 +0545

    animations

--- js/script.js
+++ js/script.js
@@ -24,6 +24,8 @@ window.addEventListener("click",function(){
 
 
 
+
+//sticky effect
 window.addEventListener("scroll",function(){
     if (window.scrollY>30){
         document.querySelector(".navbar").classList.add("sticky");
@@ -37,41 +39,102 @@ const togglebtn = document.querySelector(".toggle-btn");
 const togglebtnic = document.querySelector(".togle-btn i")
 
 togglebtn.addEventListener("click", function(){
-    document.querySelector(".dropdown").classList.toggle("click");
+    document.querySelector(".responsive-dropdown").classList.toggle("click");
 })
+
+
+//navbar dropdown
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.querySelector(".dropdown-toggle");
+    const aboutPopup = document.querySelector(".dropdown-content");
+
+    aboutLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        aboutPopup.classList.toggle("dropdown-active");
+        if (aboutPopup.classList.contains("dropdown-active")) {
+            aboutLink.style.border = "2px solid var(--content-color)";
+            aboutLink.style.backgroundColor = "var(--content-color)";
+            aboutLink.style.color = "var(--secondary)";
+        } else {
+            aboutLink.style.border = "";
+            aboutLink.style.backgroundColor = "";
+            aboutLink.style.color = "";
+        }
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!aboutLink.contains(event.target) && !aboutPopup.contains(event.target)) {
+            aboutPopup.classList.remove("dropdown-active");
+            aboutLink.style.border = "";
+            aboutLink.style.backgroundColor = "";
+            aboutLink.style.color = "";
+        }
+    });
+});
+
+
+//typing
+const changingTextElement = document.getElementById("changing-text");
+const cursorElement = document.createElement("span");
+cursorElement.classList.add("cursor");
+changingTextElement.parentNode.insertBefore(cursorElement, changingTextElement.nextSibling);
+
+const phrases = [
+    "ENGINEERING, TECHNOLOGY, AND INNOVATION",
+    "HOSTED BY KATHMANDU UNIVERSITY"
+];
+
+const colors = [
+    "#7f6ee1",
+    "#ffff"
+];
+
+let phraseIndex = 0;
+let letterIndex = 0;
+let isDeleting = false;
+
+function updateChangingText() {
+    const currentPhrase = phrases[phraseIndex];
+    const currentColor = colors[phraseIndex];
+    const currentText = currentPhrase.slice(0, letterIndex);
+
+    changingTextElement.innerHTML = currentText;
+    changingTextElement.style.color = currentColor;
+
+    if (!isDeleting && letterIndex === currentPhrase.length) {
+        setTimeout(() => {
+            isDeleting = true;
+        }, 2000);
+    } else if (isDeleting && letterIndex === 0) {
+        isDeleting = false;
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+    }
+
+    if (!isDeleting) {
+        letterIndex++;
+    } else {
+        letterIndex--;
+    }
+
+    setTimeout(updateChangingText, isDeleting ? 50 : 200);
+}
+
+updateChangingText();
+
 
 
 
 //popup image
 const popupButton = document.getElementById("popupButton");
-  const imageContainer = document.getElementById("imageContainer");
+const imageContainer = document.getElementById("imageContainer");
   
-  popupButton.addEventListener("click", () => {
+popupButton.addEventListener("click", () => {
     imageContainer.style.display = "flex";
 });
   
   imageContainer.addEventListener("click", () => {
     imageContainer.style.display = "none";
 });
-
-//facebook api
-
-// window.fbAsyncInit = function() {
-//     FB.init({
-//       appId: 'YOUR_APP_ID',
-//       autoLogAppEvents: true,
-//       xfbml: true,
-//       version: 'v12.0'
-// });
-// };
-  
-// (function(d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) return;
-//     js = d.createElement(s); js.id = id;
-//     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
 
 
 //scroll animation
